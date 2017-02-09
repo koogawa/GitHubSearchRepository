8b412169f9616988f4c00f58b80394cde5efb72e
koogawa <koogawa.app@gmail.com>
2017-02-09 17:21:10 +0900

    Add JSONDecodeError

---
+++ GitHubSearchRepository/Error/JSONDecodeError.swift
@@ -0,0 +1,14 @@
+//
+//  JSONDecodeError.swift
+//  GitHubSearchRepository
+//
+//  Created by koogawa on 2017/02/09.
+//  Copyright © 2017年 koogawa. All rights reserved.
+//
+
+import Foundation
+
+enum JSONDecodeError : Error {
+    case invalidFormat(json: Any)
+    case missingValue(key: String, actualValue: Any?)
+}
