83f486984c2af7d256713f038e2462870c41d2cd
koogawa <koogawa.app@gmail.com>
2017-02-09 17:21:41 +0900

    Add JSONDecodable

---
+++ GitHubSearchRepository/Response/JSONDecodable.swift
@@ -0,0 +1,13 @@
+//
+//  JSONDecodable.swift
+//  GitHubSearchRepository
+//
+//  Created by koogawa on 2017/02/09.
+//  Copyright © 2017年 koogawa. All rights reserved.
+//
+
+import Foundation
+
+protocol JSONDecodable {
+    init(json: Any) throws
+}
